c0253d5159b0c279bb42cc5da79313c86da9d060
Cesar Carvalho <araujo302@gmail.com>
2021-03-15 10:59:49 -0300

    Add ConsoleVB

--- ConsoleAppVB/ConsoleAppVB/Module1.vb
+++ ConsoleAppVB/ConsoleAppVB/Module1.vb
@@ -1,6 +1,30 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim valorDecimal As Decimal?
+
+        Dim valorString As String = "5459,33"
+        Dim resultadoDecimal As String
+        Dim resultadoString As String
+        ' User-defined numeric formats.
+        resultadoDecimal = Format(valorDecimal, "##,##0")
+
+        Dim testes = IsNothing(valorDecimal)
+
+        If (String.IsNullOrEmpty(valorString)) Then
+            resultadoString = "0"
+        Else
+            resultadoString = Format(Convert.ToDecimal(valorString), "##,##0")
+        End If
+
+        Convert.ToDecimal(valorString)
+
+        Dim test = FormatCurrency(valorString, , TriState.False, TriState.False, TriState.False)
+
+        FormatCurrency(123.456, 0)
+
+        Console.WriteLine($"Resultado convertendo Decimal : {resultadoDecimal} .")
+        Console.WriteLine($"Resultado convertendo String : {resultadoString} .")
 
     End Sub
 
